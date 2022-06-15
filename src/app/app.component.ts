dc1b9198ac04e6ed04d6b6fd6ef7e174f0b264f5
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-06-15 11:56:49 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'mathform';
+
+  // Section 20: Build a Fast-Paced Math Game
+  // 253. App Overview
+  
+}
